b3f39c351f1c737476a175ad8e6cba4f487685fc
Jyotsana_Srivastava <iit2019174@iiita.ac.in>
2021-03-24 09:51:48 +0530

    CarActivity - Comments ki safai

--- app/src/main/java/com/example/nomads/CarActivity.java
+++ app/src/main/java/com/example/nomads/CarActivity.java
@@ -33,6 +33,8 @@ public class CarActivity extends AppCompatActivity {
     DatabaseReference userRef;
     String currentUserID;
 
+    //private static final String TAG = CarActivity.class.getSimpleName();
+    //private ImageView carImage;
     private TextView car_plateNoTv, car_modelNoTv, car_mileageTv, car_ratingTv, car_availabilityTv;
 
     @Override
@@ -49,6 +51,7 @@ public class CarActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Car Profile");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        //carImage = (ImageView)findViewById(R.id.car_imageView);
         bookButton = (AppCompatButton)findViewById(R.id.car_book_button);
         availableTv = (TextView)findViewById(R.id.car_available_text_view);
         waitingTv = (TextView)findViewById(R.id.car_waiting_text_view);
@@ -62,7 +65,6 @@ public class CarActivity extends AppCompatActivity {
 
         carId = getIntent().getStringExtra("carId");
         isThreshPaymentSuccessfull = getIntent().getIntExtra("bookedSuccess", 0);
-        //Toast.makeText(this, "String extra from intent: " + carId, Toast.LENGTH_SHORT).show();
 
         carRef = FirebaseDatabase.getInstance().getReference().child("Cars").child(carId);
         //Display details
@@ -90,7 +92,6 @@ public class CarActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 carAvailable = snapshot.child("available").getValue().toString();
-                //Toast.makeText(CarActivity.this, "Available read from firebase: "+available, Toast.LENGTH_SHORT).show();
                 if(carAvailable.equals("true")){
                     bookButton.setEnabled(true);
                     //availableTv.setText("Car Unavailable");
@@ -224,108 +225,3 @@ public class CarActivity extends AppCompatActivity {
         userRef.child("Car booked").child(carId).child("waiting time").setValue(str);
     }
 }
-/*
-=======
-package com.example.nomads;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import androidx.appcompat.widget.AppCompatButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-
-public class CarActivity extends AppCompatActivity {
-
-    private static final String TAG = CarActivity.class.getSimpleName();
-
-    private ImageView carImage;
-    private AppCompatButton bookingButton;
-
-
-    private Toolbar mToolbar;
-
-    private FirebaseAuth mAuth;
-    private DatabaseReference profileUserRef;
-    String currentUserID;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_car);
-
-        mAuth = FirebaseAuth.getInstance();
-        currentUserID = mAuth.getCurrentUser().getUid();
-        profileUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserID);
-
-        mToolbar = (Toolbar)findViewById(R.id.profile_toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("CarProfile");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        car_plateNoTv = (TextView)findViewById(R.id.car_plateNo);
-        car_modelNoTv = (TextView)findViewById(R.id.car_modelNo);
-        car_mileageTv = (TextView)findViewById(R.id.car_mileage);
-        car_ratingTv = (TextView)findViewById(R.id.car_rating);
-        car_availabilityTv = (TextView)findViewById(R.id.car_availability);
-
-        carImage = (ImageView)findViewById(R.id.car_imageView);
-        AppCompatButton bookingButton = (AppCompatButton)findViewById(R.id.car_book_button);
-
-
-        profileUserRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
-
-                    String car_plateNo = snapshot.child("car_plateNo").getValue().toString();
-                    String car_modelNo = snapshot.child("car_modelNo").getValue().toString();
-                    String car_mileage = snapshot.child("car_mileage").getValue().toString();
-                    String car_rating = snapshot.child("car_rating").getValue().toString();
-                    String car_availability = snapshot.child("car_availability").getValue().toString();
-
-                    car_plateNoTv.setText(car_plateNo);
-                    car_modelNoTv.setText(car_modelNo);
-                    car_mileageTv.setText(car_mileage);
-                    car_ratingTv.setText(car_rating);
-                    car_availabilityTv.setText(car_availability);
-                }
-                else{
-                    Toast.makeText(CarActivity.this, "Snapshot doesn't exist", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        bookingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent bookingIntent = new Intent(CarActivity.this, MainActivity.class);
-                startActivity(bookingIntent);
-            }
-        });
-    }
-
-}
-
->>>>>>> upstream/main
-
- */
